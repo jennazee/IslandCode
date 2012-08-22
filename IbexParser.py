77e853ee2ccb64d8a2a54330d7db6bcbc4537777
Jenna Zeigen <jenna.zeigen@gmail.com>
2012-08-22 16:55:56 -0400

    parser now scrapes from internet

--- IbexParser.py
+++ IbexParser.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python
 
-import re, sys, string
+import re, sys, string, urllib2
 
-# def get_data_file(url, data=None, headers={}):
-# 	req = urllib2.Request(url=url)
-# 	for key in headers:
-# 		req.add_header(key, headers[key])
-# 	site = urllib2.urlopen(req)
-# 	data = site.read()
-# 	headers = site.info()
-# 	site.close()
-# 	output = open('data.txt', 'w')
-# 	output.write(data)
-# 	output.close()
-# 	return data, headers
+local_file = 'data.txt'
+
+def get_data_file(url, data=None, headers={}):
+	req = urllib2.Request(url=url)
+	for key in headers:
+		req.add_header(key, headers[key])
+	site = urllib2.urlopen(req)
+	data = site.read()
+	headers = site.info()
+	site.close()
+	output = open(local_file, 'w')
+	output.write(data)
+	output.close()
+	return data, headers
 
 def parse_results_file(filename):
 	"""
@@ -28,9 +30,9 @@ def parse_results_file(filename):
 	processed = []
 	languages = 'NONE'
 	ID = 'NONE'
-
-	#moreheader = raw_input('Extra header labels from question field (e.g.: item,condition,factor1,factor2): ')
-	#stim_type = raw_input('What type are your stims? (i.e. AcceptabilityJudgment): ')
+	
+	moreheader = raw_input('Extra header labels from question field (e.g.: item,condition,factor1,factor2): ')
+	stim_type = raw_input('What type are your stims? (i.e. AcceptabilityJudgment): ')
 	output_loc = raw_input('Where would you like to put your parsed file? (enter filename path): ')
 	
 	for line in pretext:
@@ -45,10 +47,10 @@ def parse_results_file(filename):
 	for line in range(len(text)):
 		#get their info
 		if re.search('Form', text[line]):
-			if re.search('ID', text[line]):
-				ID = re.split('ID,', text[line])[1].strip()
-			elif re.search('languages', text[line]):
-				languages = re.split('languages,', text[line])[1].strip()
+			if re.search('number', text[line]):
+				ID = re.split('number,', text[line])[1].strip()
+			elif re.search('age', text[line]):
+				languages = re.split('age,', text[line])[1].strip()
 		if re.search('AcceptabilityJudgment', text[line]):
 			if first:
 				#print 'first'
@@ -75,16 +77,26 @@ def parse_results_file(filename):
 			
 	output = open(output_loc, 'w')
 
-	header = 'ID,Languages,Time sent,MD5 Hash of IP Address,Controller,Item Number,Element Number,Type,Group,Stimulus,Answer,RT,N,V'
+	header = 'ID,Languages,Time sent,MD5 Hash of IP Address,Controller,Item Number,Element Number,Type,Group,Stimulus,Answer,RT,'
 
-	#output.write(str(header+moreheader+'\n'))
-
-	output.write(str(header+'\n'))
+	output.write(str(header+moreheader+'\n'))
 
 	for line in processed:
 		output.write(line)
 	output.close()
 
-parse_results_file(sys.argv[1])
 
+
+#########################################
+
+online = raw_input('Is your data file on the internet? (y/n): ')
+print online == 'y'
+if online is 'y':
+	online_file = raw_input('What\'s the URL where is your data file located?: ')
+	local_file = raw_input('Where would you like to put your raw file? (enter filename path): ')
+	get_data_file(online_file)
+else:
+	local_file = raw_input('Where is your raw data file? (enter filename path): ')
+
+parse_results_file(local_file)
 print "All done! Enjoy your clean data!"
